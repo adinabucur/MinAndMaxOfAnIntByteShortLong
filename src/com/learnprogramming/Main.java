5e285fc0e531c9c57ed3ba467b2959c3c6070fde
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-07-06 10:22:58 +0300

    Byte Short Long
    The maximum and minimum of an byte, short and long

--- src/com/learnprogramming/Main.java
+++ src/com/learnprogramming/Main.java
@@ -12,7 +12,24 @@ public class Main {
 
         int myMaxIntTest = 2_147_483_647;
         System.out.println("test = " + myMaxIntValue);
-        
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value =" + myMinByteValue);
+        System.out.println("Byte Maximum Value =" + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value =" + myMinShortValue);
+        System.out.println("Short Maximum Value =" + myMaxShortValue);
+
+        long myLongValue = 100L;
+        System.out.println(myLongValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value =" + myMinLongValue);
+        System.out.println("Long Maximum Value =" + myMaxLongValue);
     }
 
 }
